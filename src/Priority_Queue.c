b295047cdf07bd5606be65aace4cceef801ca9ba
Robinbird01 <69393838+Robinbirdd@users.noreply.github.com>
2023-10-24 19:48:05 +0800

    build Priority_Queue by Heap

--- src/Priority_Queue.c
+++ src/Priority_Queue.c
@@ -2,72 +2,90 @@
 #include <stdlib.h>
 #include <string.h>
 
-// Priority Queue
-typedef struct _PQ {
-    int size;
+typedef struct Heap {
     int *arr;
-} PQ;
+    int size;
+} Heap; 
 
-// Create Priority Queue
-PQ *createPQ(int size) {
-    PQ *pq = (PQ *)malloc(sizeof(PQ));
-    pq->size = 0;
-    pq->arr = (int *)malloc(sizeof(int) * size);
+Heap *creatHeap(int size) {
+    Heap *heap = (Heap *)malloc(sizeof(Heap));
+    heap->arr = (int *)malloc(sizeof(int) * size);
+    heap->size = -1;
 
-    return pq;
+    return heap;
 }
 
-// Push
-void push(PQ *pq, int data) {
-    int idx = pq->size + 1;
+int parent(int i) {
+    return (i-1) / 2;
+}
 
-    while (idx != 1) {
-        if (pq->arr[idx / 2] < data) {
-            pq->arr[idx] = pq->arr[idx / 2];
-            idx /= 2;
-        } else {
-            break;
-        }
+int leftChild(int i) {
+    return 2 * i + 1;
+}
+int rightChild(int i) {
+    return 2 * i + 2;
+}
+
+void shiftUp(Heap *heap, int i) {
+    while(i > 0 && heap->arr[parent(i)] > heap->arr[i]) {
+        int temp = heap->arr[parent(i)];
+        heap->arr[parent(i)] = heap->arr[i];
+        heap->arr[i] = temp;
+        i = parent(i);
     }
-
-    pq->arr[idx] = data;
-    pq->size++;
 
     return;
 }
 
-// Pop
-int pop(PQ *pq) {
-    int ret = pq->arr[1];
-    int last = pq->arr[pq->size];
-    int parent = 1, child = 2;
+void shiftDown(Heap *heap, int i) {
+    int MinIndex = i;
+    int child;
+    if(leftChild(i) > rightChild(i)) {
+        child = rightChild(i);
+    } else {
+        child = leftChild(i);
+    }
+    if(child <= heap->size && heap->arr[child] < heap->arr[MinIndex]) {
+        MinIndex = child;
+    }
+    if(MinIndex != i) {
+        int temp = heap->arr[MinIndex];
+        heap->arr[MinIndex] = heap->arr[i];
+        heap->arr[i] = temp;
+        shiftDown(heap, MinIndex);
+    }
 
-    while (child < pq->size) {
-        if(child + 1 < pq->size && pq->arr[child] < pq->arr[child + 1]) {
-            child++;
-        }
+    return;
+}
 
-        if(last < pq->arr[child]) {
-            pq->arr[parent] = pq->arr[child];
-            parent = child;
-            child *= 2;
-        } else {
+void insert(Heap *heap, int data) {
+    heap->size++;
+    heap->arr[heap->size] = data;
+    shiftUp(heap, heap->size);
+}
+
+int Poll(Heap *heap) {
+    int result = heap->arr[0];
+    heap->arr[0] = heap->arr[heap->size];
+    heap->size--;
+    shiftDown(heap, 0);
+
+    return result;
+}
+
+void remove(Heap *heap, int data) {
+    int i;
+    for(i = 0; i <= heap->size; i++) {
+        if(heap->arr[i] == data) {
             break;
         }
-
-        pq->arr[parent] = last;
-
-        pq->size--;
-        return ret;
     }
-}
 
-// Top
-int top(PQ *pq) {
-    return pq->arr[1];
-}
+    heap->arr[i] = heap->arr[heap->size];
+    heap->size--;
+    
+    shiftDown(heap, i);
+    shiftUp(heap, i);
 
-// Empty
-int empty(PQ *pq) {
-    return pq->size == 0;
+    return;
 }
